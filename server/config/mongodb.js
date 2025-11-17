d30e7156346a84730af50014bdf3e40b4ccfe1f0
lac641 <lawrenceleo579@gmail.com>
2025-11-18 00:34:32 +0100

    updated

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const connectDB = async ()=>{
     try {
-        await mongoose.connect(`${process.env.MONGO_URI}`)
+        await mongoose.connect(`${process.env.MONGO_URI}/nestorria`)
         console.log('✅ Database connected');
         
     } catch (error) {
